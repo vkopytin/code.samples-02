36557527a7fbebfc58e4d88ccfe13d235c5667f5
Volodymyr K <volodymyr.kopytin@rebelmouse.com>
2024-06-21 00:03:21 +0200

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { ArticlesService } from '../services/articles.service';
 import { lastValueFrom } from 'rxjs';
@@ -10,14 +10,19 @@ import { lastValueFrom } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent extends OnInit {
+  allArticles = this.articles.lastArticles;
+
   constructor(public articles: ArticlesService) {
 
   }
 
+  ngOnInit(): void {
+    this.listArticles():
+  }
+
   async listArticles(): Promise<void> {
     const res$ = this.articles.listArticles();
-    const articles = await lastValueFrom(res$);
-    console.log(articles);
+    this.allArticles = await lastValueFrom(res$);
   }
 }
