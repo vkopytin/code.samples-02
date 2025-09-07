a6b60a6d2c880a91d8b24c4bd598a8ff0efd9542
Volodymyr Kopytin <4933561+vkopytin@users.noreply.github.com>
2025-09-07 20:11:03 +0200

    Enhance ArticleBlocksService tests: add HttpClientTestingModule to improve testing capabilities

--- src/app/services/articleBlocks.service.spec.ts
+++ src/app/services/articleBlocks.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ArticleBlocksService } from './articleBlocks.service';
 
 describe('ArticleBlocksService', () => {
   let service: ArticleBlocksService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ArticleBlocksService);
   });
 
