e53a3feb04e8c248530e83e009fc07ce2ec73b25
Volodymyr Kopytin <4933561+vkopytin@users.noreply.github.com>
2025-10-07 20:30:04 +0200

    making nice items render

--- src/app/catalog/subscriptions/subscription.model.ts
+++ src/app/catalog/subscriptions/subscription.model.ts
@@ -1,21 +1,12 @@
-export interface SnippetThumbnails {
-  default: { url: string; };
-  medium: { url: string; };
-  high: { url: string; };
-}
-
-export interface SubsSnippet {
-  title: string;
-  description: string;
-  thumbnails: SnippetThumbnails
-}
-
 export interface SubscriptionItem {
   id: string;
-  snippet: SubsSnippet;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+  updatedAt: string;
 }
 
 export interface SubscriptionResponse {
   items: SubscriptionItem[];
 }
-
