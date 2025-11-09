95698b238165cf0be97032013da263ab04fc1687
Volodymyr Kopytin <4933561+vkopytin@users.noreply.github.com>
2025-11-09 16:02:45 +0100

    changing algorithm to preview media

--- src/app/article-editor/article-editor.component.ts
+++ src/app/article-editor/article-editor.component.ts
@@ -55,22 +55,7 @@ export class ArticleEditorComponent implements OnInit{
 
   togglePrintMediaView(): void {
     const src = this._article.media?.sourceUrl;
-    var win = window.open('about:blank', "_new");
-    if (!win) {
-      window.open(src, "_blank");
-      return;
-    }
-    win.document.open();
-    win.document.write([
-        '<html>',
-        '   <head>',
-        '   </head>',
-        '   <body onload="window.print()" onafterprint="window.close()">',
-        '       <img src="' + src + '"/>',
-        '   </body>',
-        '</html>'
-    ].join(''));
-    win.document.close();
+    window.open(src, "_new");
   }
 
   onTitleChange(newTitle: string): void {
