f769e15eba4e2c21bd61aada72cd0df20d316b8d
Volodymyr Kopytin <4933561+vkopytin@users.noreply.github.com>
2025-11-16 17:21:50 +0100

    FIx autocolsing dropdowns on outside click

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,21 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// Enable :active styles on mobile devices
 document.addEventListener('touchstart', () => {}, true);
+
+// Close dropdowns when clicking outside
+window.addEventListener('click', e => {
+  document.querySelectorAll('.dropdown').forEach((dropdown: Element & { open?: boolean }) => {
+    if (!('target' in e) || !e.target) {
+      return;
+    }
+    if (!dropdown.contains(e.target as Node)) {
+      // Click was outside the dropdown, close it
+      dropdown.open = false;
+    }
+  });
+});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
