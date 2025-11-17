09cc3496ba8cf479fba9468db79b5792919c85aa
Volodymyr Kopytin <4933561+vkopytin@users.noreply.github.com>
2025-11-17 10:53:24 +0100

    Fix unit tests

--- src/app/article-editor/article-media/article-media.component.spec.ts
+++ src/app/article-editor/article-media/article-media.component.spec.ts
@@ -1,5 +1,6 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ArticleMediaComponent } from './article-media.component';
 
 describe('ArticleMediaComponent', () => {
@@ -8,7 +9,10 @@ describe('ArticleMediaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ArticleMediaComponent]
+      imports: [ArticleMediaComponent],
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting(),
+      ]
     })
     .compileComponents();
 
